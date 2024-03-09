a4764f50f96479d424d619929e94ca47a81d05ef
Luis Antonio <67035364+LuisAntonioFreitas@users.noreply.github.com>
2024-03-09 21:32:15 +0000

    Sat, Mar 9, 2024, 6:32 PM -03:00

--- laboratorios_python/modulo6_01_tournament/lab6/tournament.py
+++ laboratorios_python/modulo6_01_tournament/lab6/tournament.py
@@ -71,6 +71,9 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
+    while len(teams) > 1:
+        teams = simulate_round(teams)
+    return teams[0]["team"]
 
 
 if __name__ == "__main__":
