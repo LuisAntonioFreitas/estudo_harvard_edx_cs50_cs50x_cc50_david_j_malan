3ff989fdefa2efe9aeffe04bdb9b14d42daa066a
Luis Antonio <67035364+LuisAntonioFreitas@users.noreply.github.com>
2024-03-14 19:46:24 +0000

    Thu, Mar 14, 2024, 4:46 PM -03:00

---
+++ exercicios_sql/modulo7_01_moveis/movies/1.sql
@@ -0,0 +1,4 @@
+--Em 1.sql, escreva uma consulta SQL para listar os títulos de todos os filmes lançados em 2008.
+--Sua consulta deve gerar uma tabela com uma única coluna para o título de cada filme.
+
+SELECT 
