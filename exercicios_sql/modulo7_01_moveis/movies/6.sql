cb678fe7cb75ee1142a3658bd90ab37566bad013
Luis Antonio <67035364+LuisAntonioFreitas@users.noreply.github.com>
2024-03-14 20:01:05 +0000

    Thu, Mar 14, 2024, 5:01 PM -03:00

---
+++ exercicios_sql/modulo7_01_moveis/movies/6.sql
@@ -0,0 +1,4 @@
+--Em 6.sql, escreva uma consulta SQL para determinar a avaliação média de todos os filmes lançados em 2012.
+--Sua consulta deve gerar uma tabela com uma única coluna e uma única linha (sem incluir o cabeçalho) contendo a classificação média.
+
+SELECT  
