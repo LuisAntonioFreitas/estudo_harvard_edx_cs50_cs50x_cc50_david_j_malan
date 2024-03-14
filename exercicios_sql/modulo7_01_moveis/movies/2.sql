fb682cf47679ba9fc964baf3f38ac7def3fbec6c
Luis Antonio <67035364+LuisAntonioFreitas@users.noreply.github.com>
2024-03-14 19:50:41 +0000

    Thu, Mar 14, 2024, 4:50 PM -03:00

---
+++ exercicios_sql/modulo7_01_moveis/movies/2.sql
@@ -0,0 +1,4 @@
+--Em 2.sql, escreva uma consulta SQL para determinar o ano de nascimento de Emma Stone.
+--Sua consulta deve gerar uma tabela com uma única coluna e uma única linha (sem incluir o cabeçalho) contendo o ano de nascimento de Emma Stone.
+--Você pode presumir que há apenas uma pessoa no banco de dados com o nome Emma Stone.
+
