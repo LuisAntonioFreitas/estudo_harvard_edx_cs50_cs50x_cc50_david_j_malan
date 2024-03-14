7a30897c774230914f914ab4ba21f85d403599f2
Luis Antonio <67035364+LuisAntonioFreitas@users.noreply.github.com>
2024-03-14 22:47:55 +0000

    Thu, Mar 14, 2024, 7:47 PM -03:00

--- exercicios_sql/modulo7_01_moveis/movies/10.sql
+++ exercicios_sql/modulo7_01_moveis/movies/10.sql
@@ -2,4 +2,6 @@
 --Sua consulta deve gerar uma tabela com uma única coluna para o nome de cada pessoa.
 --Se uma pessoa dirigiu mais de um filme que recebeu uma classificação de pelo menos 9,0, eles só devem aparecer em seus resultados uma vez.
 
-SELECT  
+SELECT DISTINCT
+        people.name
+FROM    people
