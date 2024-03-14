dfc8de665d82c22b6c3f00aadfcaecd69d55f94e
Luis Antonio <67035364+LuisAntonioFreitas@users.noreply.github.com>
2024-03-14 22:20:03 +0000

    Thu, Mar 14, 2024, 7:20 PM -03:00

--- exercicios_sql/modulo7_01_moveis/movies/6.sql
+++ exercicios_sql/modulo7_01_moveis/movies/6.sql
@@ -1,4 +1,8 @@
 --Em 6.sql, escreva uma consulta SQL para determinar a avaliação média de todos os filmes lançados em 2012.
 --Sua consulta deve gerar uma tabela com uma única coluna e uma única linha (sem incluir o cabeçalho) contendo a classificação média.
 
-SELECT  
+SELECT  AVG(rating)
+FROM    movies
+JOIN    ratings
+        ON  movies.id = ratings.movie_id
+WHERE   ( year = 2012 )
