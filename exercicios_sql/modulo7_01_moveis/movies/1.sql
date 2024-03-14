675bfd11d88c97df731faddce079d12987fd0812
Luis Antonio <67035364+LuisAntonioFreitas@users.noreply.github.com>
2024-03-14 19:47:53 +0000

    Thu, Mar 14, 2024, 4:47 PM -03:00

--- exercicios_sql/modulo7_01_moveis/movies/1.sql
+++ exercicios_sql/modulo7_01_moveis/movies/1.sql
@@ -1,4 +1,4 @@
 --Em 1.sql, escreva uma consulta SQL para listar os títulos de todos os filmes lançados em 2008.
 --Sua consulta deve gerar uma tabela com uma única coluna para o título de cada filme.
 
-SELECT 
+SELECT title FROM moveis WHERE year = 2008;
