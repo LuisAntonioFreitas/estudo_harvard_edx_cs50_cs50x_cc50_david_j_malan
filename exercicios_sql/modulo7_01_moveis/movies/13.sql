b53895bc8e45f02335045e83d06b61464f83feba
Luis Antonio <67035364+LuisAntonioFreitas@users.noreply.github.com>
2024-03-15 00:14:31 +0000

    Thu, Mar 14, 2024, 9:14 PM -03:00

--- exercicios_sql/modulo7_01_moveis/movies/13.sql
+++ exercicios_sql/modulo7_01_moveis/movies/13.sql
@@ -4,7 +4,7 @@
 --O próprio Kevin Bacon não deve ser incluído na lista resultante.
 
 SELECT DISTINCT
-        p1.name
+        p2.name
 FROM    movies
 JOIN    stars
         ON  movies.id = stars.movie_id
@@ -12,5 +12,5 @@ JOIN    people p1
         ON  stars.person_id = p1.id
 JOIN    people p2
         ON  stars.person_id = p2.id
-WHERE   ( p2.name IN ('Kevin Bacon') )
-AND     ( NOT p1.name IN ('Kevin Bacon') );
+WHERE   ( p1.name IN ('Kevin Bacon') )
+AND     ( NOT p2.name IN ('Kevin Bacon') );
