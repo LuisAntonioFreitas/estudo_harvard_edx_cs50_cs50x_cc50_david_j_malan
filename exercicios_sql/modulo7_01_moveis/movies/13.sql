973353b87335bcd3f9627f6bf935f836f048436b
Luis Antonio <67035364+LuisAntonioFreitas@users.noreply.github.com>
2024-03-15 00:11:04 +0000

    Thu, Mar 14, 2024, 9:11 PM -03:00

--- exercicios_sql/modulo7_01_moveis/movies/13.sql
+++ exercicios_sql/modulo7_01_moveis/movies/13.sql
@@ -3,7 +3,8 @@
 --Pode haver várias pessoas chamadas Kevin Bacon no banco de dados. Certifique-se de selecionar apenas Kevin Bacon nascido em 1958.
 --O próprio Kevin Bacon não deve ser incluído na lista resultante.
 
-SELECT  people.name
+SELECT DISTINCT
+        people.name
 FROM    movies
 JOIN    stars
         ON  movies.id = stars.movie_id
