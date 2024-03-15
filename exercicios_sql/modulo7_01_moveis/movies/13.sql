59c6c1b36202920f72965945b5979416e8a006be
Luis Antonio <67035364+LuisAntonioFreitas@users.noreply.github.com>
2024-03-15 00:09:56 +0000

    Thu, Mar 14, 2024, 9:09 PM -03:00

--- exercicios_sql/modulo7_01_moveis/movies/13.sql
+++ exercicios_sql/modulo7_01_moveis/movies/13.sql
@@ -9,6 +9,6 @@ JOIN    stars
         ON  movies.id = stars.movie_id
 JOIN    people
         ON  stars.person_id = people.id
-WHERE   ( people.name IN ('Johnny Depp', 'Helena Bonham Carter') )
+WHERE   ( people.name IN ('Kevin Bacon') )
 GROUP BY movies.id
 HAVING  COUNT(DISTINCT people.name) = 2;
