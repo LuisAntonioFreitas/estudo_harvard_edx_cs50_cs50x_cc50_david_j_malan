d0618bb893373308e00a6528c619e5df9f736ed7
Luis Antonio <67035364+LuisAntonioFreitas@users.noreply.github.com>
2024-03-15 00:17:21 +0000

    Thu, Mar 14, 2024, 9:17 PM -03:00

--- exercicios_sql/modulo7_01_moveis/movies/13.sql
+++ exercicios_sql/modulo7_01_moveis/movies/13.sql
@@ -3,14 +3,12 @@
 --Pode haver várias pessoas chamadas Kevin Bacon no banco de dados. Certifique-se de selecionar apenas Kevin Bacon nascido em 1958.
 --O próprio Kevin Bacon não deve ser incluído na lista resultante.
 
-SELECT DISTINCT
+SELECT 
         p2.name
-FROM    movies
-JOIN    stars
-        ON  movies.id = stars.movie_id
+FROM    stars
 JOIN    people p1
         ON  stars.person_id = p1.id
+        AND p1.name IN ('Kevin Bacon')
 JOIN    people p2
         ON  stars.person_id = p2.id
-WHERE   ( p1.name IN ('Kevin Bacon') )
-AND     ( NOT p2.name IN ('Kevin Bacon') );
+        AND NOT p2.name IN ('Kevin Bacon');
