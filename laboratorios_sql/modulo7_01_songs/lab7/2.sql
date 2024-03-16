93a0294df20deb9de54baf673cccb842c43d07cd
Luis Antonio <67035364+LuisAntonioFreitas@users.noreply.github.com>
2024-03-16 01:06:57 +0000

    Fri, Mar 15, 2024, 10:06 PM -03:00

--- laboratorios_sql/modulo7_01_songs/lab7/2.sql
+++ laboratorios_sql/modulo7_01_songs/lab7/2.sql
@@ -1,4 +1,6 @@
 --No 2.sql, escreva uma consulta SQL para listar os nomes de todas as músicas em ordem crescente de ritmo.
 --Sua consulta deve gerar uma tabela com uma única coluna para o nome de cada música.
 
-SELECT
+SELECT  name
+FROM    songs
+ORDER BY tempo;
