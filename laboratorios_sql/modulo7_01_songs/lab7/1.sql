614ffd817c5ec4815300b980a479a80755fe5004
Luis Antonio <67035364+LuisAntonioFreitas@users.noreply.github.com>
2024-03-16 01:03:41 +0000

    Fri, Mar 15, 2024, 10:03 PM -03:00

--- laboratorios_sql/modulo7_01_songs/lab7/1.sql
+++ laboratorios_sql/modulo7_01_songs/lab7/1.sql
@@ -1,4 +1,5 @@
 --No 1.sql, escreva uma consulta SQL para listar os nomes de todas as músicas no banco de dados.
 --Sua consulta deve gerar uma tabela com uma única coluna para o nome de cada música.
 
-SELECT
+SELECT  name
+FROM    songs;
