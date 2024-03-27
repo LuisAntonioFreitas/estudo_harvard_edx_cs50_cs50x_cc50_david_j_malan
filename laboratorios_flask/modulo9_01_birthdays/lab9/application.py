caa54dedc1185eef7890fd482feb0016b785bac1
Luis Antonio <67035364+LuisAntonioFreitas@users.noreply.github.com>
2024-03-27 03:29:38 +0000

    Wed, Mar 27, 2024, 12:29 AM -03:00

--- laboratorios_flask/modulo9_01_birthdays/lab9/application.py
+++ laboratorios_flask/modulo9_01_birthdays/lab9/application.py
@@ -17,13 +17,20 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
+        # return redirect("/")
 
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        db.execute(
+            "INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day
+        )
         return redirect("/")
 
     else:
 
         # TODO: Display the entries in the database on index.html
+        # return render_template("index.html")
 
-        return render_template("index.html")
-
-
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", birthdays=birthdays)
